c7cfce4e21326da9c1a63bdbbaf42b32049304d5
Diego Giacobo <diegogiacobbo@gmail.com>
2021-02-08 09:08:01 -0500

    Start templetate form (validate) with angular.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { GithubComponent } from './github/github.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
 const APP_ROUTES: Routes = [
-  {path: 'github', component: GithubComponent}
+  {path: 'github', component: GithubComponent},
+  {path: 'formulaire', component: TemplateFormComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  constructor(private router: Router) {
+    this.router.errorHandler = (error: any) => {
+        console.log(error);
+        // this.router.navigate(['404']); // or redirect to default route
+    }
+
+  }
+
+ }
